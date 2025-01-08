366122bd1b3c046ce921bf32529bb7d9a3b562cc
UltraQbik <no1skill@yandex.ru>
2025-01-08 03:47:47 +0300

    Add serving stuff

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import asyncio
+import logging
 
 
 class Application:
-    def __init__(self, listening_port: int, server_overworld_port: int, server_hellworld_port: int | None = None):
+    def __init__(
+            self,
+            listening_port: int,
+            server_overworld_port: int,
+            server_hellworld_port: int | None = None,
+            binding_address: str = "0.0.0.0"
+    ):
+        self.binding_address: str = binding_address
         self.listening_port: int = listening_port
         self.overworld_port: int = server_overworld_port
         self.hellworld_port: int | None = server_hellworld_port
+
+        self.server: asyncio.Server | None = None
+
+        self.logger: logging.Logger = logging.getLogger(__name__)
 
     def run(self):
         """
@@ -13,9 +25,25 @@ class Application:
         """
 
         async def coro():
-            pass
+            self.server = await asyncio.start_server(
+                client_connected_cb=self.client_handler,
+                host=self.binding_address,
+                port=self.listening_port)
+
+            self.logger.info(f"Proxy listening on '{self.binding_address}:{self.listening_port}'")
+            async with self.server:
+                try:
+                    await self.server.serve_forever()
+                except asyncio.exceptions.CancelledError:
+                    pass
+            self.logger.info("Proxy stopped")
 
         asyncio.run(coro())
+
+    async def client_handler(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+        """
+        Handles the client connection
+        """
 
 
 def main():
