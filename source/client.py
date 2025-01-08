93607618e11f8bf82e9886665da4793a384a59f7
UltraQbik <no1skill@yandex.ru>
2025-01-08 08:27:04 +0300

    Define networking methods

--- source/client.py
+++ source/client.py
@@ -5,3 +5,26 @@ class Client:
     def __init__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
         self.reader: asyncio.StreamReader = reader
         self.writer: asyncio.StreamWriter = writer
+
+        self.server_reader: asyncio.StreamReader | None = None
+        self.server_writer: asyncio.StreamWriter | None = None
+
+        self.username: str = "undefined"
+        self.connected: bool = False
+
+    async def initial_connection(self, srv_reader: asyncio.StreamReader, srv_writer: asyncio.StreamWriter):
+        """
+        Initial connection to the server
+        :param srv_reader: server reader
+        :param srv_writer: server writer
+        """
+
+    async def cli2srv(self):
+        """
+        Client to server communication
+        """
+
+    async def srv2cli(self):
+        """
+        Server to client communication
+        """
