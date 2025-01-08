dd281e5e566b213a9a23b2b0053d0301e329130b
UltraQbik <no1skill@yandex.ru>
2025-01-08 03:40:56 +0300

    Some things

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import socket
+import asyncio
 
 
 class Application:
-    pass
+    def __init__(self, listening_port: int, server_overworld_port: int, server_hellworld_port: int | None = None):
+        self.listening_port: int = listening_port
+        self.overworld_port: int = server_overworld_port
+        self.hellworld_port: int | None = server_hellworld_port
+
+    def run(self):
+        """
+        Starts the passthrough proxy
+        """
+
+        async def coro():
+            pass
+
+        asyncio.run(coro())
 
 
 def main():
-    app = Application()
+    app = Application(
+        listening_port=25565,
+        server_overworld_port=20000)
 
 
 if __name__ == '__main__':
